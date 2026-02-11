1c56675062548fb4c4551a7d373cfd87be0f531d
MAC <mac@MACs-MacBook-Pro.local>
2026-02-11 21:14:42 +0100

    celo-sepolia

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -4,18 +4,11 @@ pragma solidity ^0.8.19;
 contract Greeter {
     string private greeting;
 
-    event GreetingChanged(string newGreeting);
-
-    constructor(string memory _greeting) {
-        greeting = _greeting;
-    }
-
-    function greet() public view returns (string memory) {
+    function getGreetings() public view returns (string memory) {
         return greeting;
     }
 
     function setGreeting(string memory _greeting) public {
         greeting = _greeting;
-        emit GreetingChanged(_greeting);
     }
 }
